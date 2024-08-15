296b27d1828e93a43a7beda254e7a284c8ae65b1
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-16 00:24:34 +0500

    Components

--- MyApp/components/mainComponents.jsx
+++ MyApp/components/mainComponents.jsx
@@ -3,14 +3,42 @@ export function App() {
     return (
     <div>
         <h1>
-        <img src="../pizzas/funghi.jpg" alt="Pizza Funghi" />
+            <Header />
+            <Menu />
+            <Footer />
         </h1>
     </div> 
     )
 }
 
 export function Pizza(){
-    return <h2>Pizza</h2>;
+    
+    return <div>
+        <h4>Pizza</h4>
+        <img src="../pizzas/funghi.jpg" alt="Pizza Funghi" width="200px"/>
+    </div> 
 }
 
 
+export function Header(){
+    return <h3>
+        Fusion Pizza Co.
+    </h3>
+}
+
+export function Menu(){
+    return (
+    <div>
+       <h3>Our Menu</h3>
+        <Pizza />
+        <Pizza />
+        <Pizza />
+    </div>
+    )
+
+    
+}
+
+export function Footer(){
+    return <footer>{new Date().toLocaleDateString() }. We are currently open. </footer>
+}
--- MyApp/src/index.jsx
+++ MyApp/src/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import pizzaData from "../public/data";
+
 import { App, Pizza } from "../components/mainComponents";
 // React Version 18 Rendering
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Pizza />
         <App /> 
     </React.StrictMode>
 );
