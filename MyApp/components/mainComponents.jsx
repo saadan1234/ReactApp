c43cd390a6d14e1dd3f005b74a05a5485408d44f
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-08-17 13:55:01 +0500

    Styling

--- MyApp/components/mainComponents.jsx
+++ MyApp/components/mainComponents.jsx
@@ -3,52 +3,70 @@ import pizzaData from '../public/data.js';
 
 export function App() {
     return (
-    <div>
-        <h1>
+        <div className="bg-yellow-100 flex flex-col min-h-screen">
             <Header />
-            <Menu />
+            {/* Flex-grow ensures Menu takes up the remaining space */}
+            <div className="flex-grow">
+                <Menu />
+            </div>
             <Footer />
-        </h1>
-    </div> 
-    )
+        </div>
+    );
 }
 
-export function Menu(){
+export function Menu() {
     return (
-    <div>
-       <h3>Our Menu</h3>
-       {pizzaData.map((pizzaData)=> {
-            // eslint-disable-next-line react/jsx-key
-            return <Pizza name={pizzaData.name} ingredients={pizzaData.ingredients} photoName={pizzaData.photoName}/>
-       })}
-       
-    </div>
-    )
+        <div className="flex flex-wrap justify-center">
+            {pizzaData.map((pizzaData) => {
+                return (
+                    <Pizza
+                        key={pizzaData.name}
+                        name={pizzaData.name}
+                        ingredients={pizzaData.ingredients}
+                        photoName={pizzaData.photoName}
+                        price={pizzaData.price}
+                        soldOut={pizzaData.soldOut}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
-export function Pizza(props){
-    console.log(props)
-    return <div style={{backgroundColor: "lightyellow"}}>
-        <img src={props.photoName} alt={props.name} width="200px"/>
-        <h6>{props.name}</h6>
-        <h6>{props.ingredients}</h6>
-    </div> 
+export function Pizza(props) {
+    return (
+        <div className="flex m-4 p-4 align-middle">
+            <img src={props.photoName} alt={props.name} width="150px" />
+            <span className="content-center ml-4 p-6 rounded bg-yellow-300 w-64">
+                <h6 className="font-bold font-serif text-2xl bg-gradient-to-t from-yellow-300 to-yellow-400">
+                    {props.name}
+                </h6>
+                <h6 className="font-serif">Ingredients: {props.ingredients}</h6>
+                <h6 className="font-serif bg-gradient-to-b from-yellow-300 to-yellow-400">
+                    Price: {props.price} $
+                </h6>
+            </span>
+        </div>
+    );
 }
 
-
-export function Header(){
-    return <header className="text-3xl font-bold underline">
-        Fusion Pizza Co.
-    </header>
+export function Header() {
+    return (
+        <header className="text-center text-3xl text-white font-bold font-serif rounded-full p-6 bg-gradient-to-t from-yellow-300 to-yellow-500">
+            Fusion Pizza Co.
+        </header>
+    );
 }
 
+export function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 23;
+    const isOpen = hour >= openHour && hour <= closeHour;
 
-
-export function Footer(){
-    // const hour = new Date().getHours();
-    // const openHour = 12;
-    // const closeHour = 23 ;
-    // const isOpen = hour>= openHour && hour<= closeHour
-    // isOpen? alert("We are currently open!") : alert("Sorry we are closed!");
-    return <footer  style={{color:"red", fontSize:"32px", backgroundColor: "black"}}> {new Date().toLocaleDateString() }. We are currently open. </footer>
+    return (
+        <footer className="text-center text-3xl text-white font-bold font-serif p-6 bg-gradient-to-b from-yellow-300 to-yellow-500">
+            {new Date().toLocaleDateString()}. {isOpen ? "We are currently open!" : "Sorry we are closed!"}
+        </footer>
+    );
 }
